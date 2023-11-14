0e6e1f78aff4d0bd11516eb83bd43336b12acc89
Réka Aranyosi <reka.aranyosi@stud.uni-corvinus.hu>
2023-11-14 09:14:58 +0100

    adatbázis létrehozva

---
+++ MS46KX_9gyak/JokeModels/Joke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MS46KX_9gyak.JokeModels;
+
+public partial class Joke
+{
+    public int JokeSk { get; set; }
+
+    public string? JokeText { get; set; }
+
+    public int UpVotes { get; set; }
+
+    public int DownVotes { get; set; }
+}
