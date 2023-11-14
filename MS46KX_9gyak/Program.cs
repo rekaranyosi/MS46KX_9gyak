826461f6b5c7b1ce32fb1ce48cdb803dbd68e55d
Réka Aranyosi <reka.aranyosi@stud.uni-corvinus.hu>
2023-11-14 09:26:58 +0100

    API Controller létrehozása

--- MS46KX_9gyak/Program.cs
+++ MS46KX_9gyak/Program.cs
@@ -2,27 +2,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers();//Az API Controllerekhez kell 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();//A Swaggerhez kell
+builder.Services.AddSwaggerGen();//A Swaggerhez kell
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())//A Swagger éles környezetben nem indul
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection();//Ha http-vel nyitják az oldalt, átirányít https-re
 
-app.UseAuthorization();
+//app.UseAuthorization();//ez nem tudom kell-e
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.MapControllers();
+app.UseDefaultFiles();       //Ha nincs semmi a domain tuán, akkor az index.html-t tölti
+app.MapControllers();        //Az API Controllereket elérhetõvé teszi
+app.UseStaticFiles();        //A wwwroot mappa tartalmát elérhetõvé teszi
 
 app.Run();
